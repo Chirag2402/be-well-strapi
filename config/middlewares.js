c755646b6500d819e5ff930b7beb0ab6e555d1f9
Chirag Khanna <chiragkhanna608@gmail.com>
2025-02-06 17:42:15 +0530

    middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,28 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "script-src": ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'], // Change to your frontend URL if needed
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
