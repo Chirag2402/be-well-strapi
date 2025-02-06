bc6c783bc2ade4651a8805ef5141dc0e85ce264c
Chirag Khanna <chiragkhanna608@gmail.com>
2025-02-06 16:10:56 +0530

    Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("dy9arv1g9"),
+          api_key: env("347432479663715"),
+          api_secret: env("V8PtzKWQdz5Vpw5gZHn94IAld-Q"),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+  });
+  
