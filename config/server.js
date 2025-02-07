dba56aff80f1ba0ebb35c0718e6ea270778c159c
Chirag Khanna <chiragkhanna608@gmail.com>
2025-02-07 16:03:37 +0530

    server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://be-well-strapi.onrender.com'), // Add this line
   app: {
     keys: env.array('APP_KEYS'),
   },
